c9e1f274b033424c0a161b71654a1b8ce876dae4
MR-alyyy <aly.matheus2342@gmail.com>
2026-08-04 10:53:45 -0300

    Refactor book registration form and logic

--- cad_livros.php
+++ cad_livros.php
@@ -1,81 +1,65 @@
+<?php
+include "conexao.php";
 
+$mensagem = "";
+
+if (isset($_POST['inserir'])) {
+
+    $titulo = trim($_POST['titulo']);
+    $autor = trim($_POST['autor']);
+    $genero = trim($_POST['genero']);
+    $quantidade = (int) trim($_POST['quantidade']);
+    $descricao = trim($_POST['descricao']);
+
+    $stmt = $conexao->prepare("INSERT INTO livros (titulo, autor, genero, quantidade, descricao) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssis", $titulo, $autor, $genero, $quantidade, $descricao);
+
+    if ($stmt->execute()) {
+        $mensagem = "<p class='sucesso'>Cadastro realizado com sucesso!</p>";
+    } else {
+        $mensagem = "<p class='erro'>Erro ao cadastrar: " . $stmt->error . "</p>";
+    }
+
+    $stmt->close();
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Cadastro livro</title>
-
-
 </head>
 
 <body>
 
 <div class="container">
 
-<form method="post">
+    <h2>Cadastro</h2>
 
-<h2>cadastro</h2>
+    <?php echo $mensagem; ?>
 
+    <form method="post">
 
-<label>titulo</label>
-<input name="titulo" type="text" >
+        <label>Título</label>
+        <input name="titulo" type="text">
 
-<label>autor</label>
-<input name="autor" type="text" >
+        <label>Autor</label>
+        <input name="autor" type="text">
 
-<label>genero</label>
-<input name="genero" type="text" >
+        <label>Gênero</label>
+        <input name="genero" type="text">
 
-<label>quantidade</label>
-<input name="quantidade" type="text" >
+        <label>Quantidade</label>
+        <input name="quantidade" type="text">
 
-<label>descricao</label>
-<input name="descricao" type="text" >
+        <label>Descrição</label>
+        <input name="descricao" type="text">
 
+        <button type="submit" name="inserir">Cadastrar</button>
 
-<button type="submit" name="inserir">Cadastrar</button>
-
-
-</form>
+    </form>
 
 </div>
 
 </body>
 </html>
-<?php
-include "conexao.php";
-
-
-
-$mensagem = "";
-
-if (isset($_POST['inserir'])) {
-
-$titulo = trim($_POST['titulo']);
-$autor = trim($_POST['autor']);
-$genero = trim($_POST['genero']);
-$quantidade = trim($_POST['quantidade']);
-$descricao = trim($_POST['descricao']);
-
-$erro = false;
-
-
-
-
-$stmt = $conexao->prepare("INSERT INTO livros (titulo, autor, genero, quantidade, descricao) VALUES (?, ?, ?, ?,?)");
-
-$stmt->bind_param("sssss", $titulo, $autor, $genero, $quantidade, $descricao);
-
-if ($stmt->execute()) {
-    $mensagem = "<p class='sucesso'>Cadastro realizado com sucesso!</p>";
-} else {
-    $mensagem = "<p class='erro'>Erro ao cadastrar: ".$stmt->error."</p>";
-}
-
-$stmt->close();
-
-}
-
-
-
-?>
